358e90582a7f555d94a9bf51bcc84bfc105d871b
jean.fechter <jean.fechter@epita.fr>
2021-11-25 16:41:41 +0100

    added execution

--- src/42sh.c
+++ src/42sh.c
@@ -1,16 +1,48 @@
+#include <ast/ast.h>
 #include <err.h>
+#include <getopt.h>
 #include <io/cstream.h>
 #include <parser/parser.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <utils/utils.h>
 #include <utils/vec.h>
+
+static struct opts *parse_opts(int argc, char **argv)
+{
+    struct opts *opts = zalloc(sizeof(struct opts));
+    static struct option long_options[] =
+    {
+        {"pretty-print", no_argument, NULL, 'p'},
+        {"c", required_argument, NULL, 'c'},
+        {NULL, 0, NULL, 0}
+    };
+    int c;
+    while ((c = getopt_long(argc, argv, "p:c:", long_options, NULL)) != -1)
+    {
+        switch (c)
+        {
+        case 'p':
+            opts->p = 1;
+            break;
+        case 'c':
+            opts->c = 1;
+            opts->input = optarg;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [OPTS] [COMMAND]\n", argv[0]);
+            break;
+        }
+    }
+    return opts;
+}
 
 /**
  * \brief Parse the command line arguments
  * \return A character stream
  */
-static struct cstream *parse_args(int argc, char *argv[])
+static struct cstream *parse_args(int argc, char *argv[], struct opts **opts)
 {
     // If launched without argument, read the standard input
     if (argc == 1)
@@ -19,21 +51,22 @@ static struct cstream *parse_args(int argc, char *argv[])
             return cstream_readline_create();
         return cstream_file_create(stdin, /* fclose_on_free */ false);
     }
+    else
+        *opts = parse_opts(argc, argv);
 
     // 42sh FILENAME
-    if (argc == 2)
+    if (argc == 2 && (*opts)->p == 0)
     {
         FILE *fp = fopen(argv[1], "r");
         if (fp == NULL)
         {
-            warn("failed to open input file");
+            warn("failed to open input filesfok");
             return NULL;
         }
 
         return cstream_file_create(fp, /* fclose_on_free */ true);
     }
 
-    fprintf(stderr, "Usage: %s [COMMAND]\n", argv[0]);
     return NULL;
 }
 
@@ -42,38 +75,47 @@ static struct cstream *parse_args(int argc, char *argv[])
  * \return An error code
  */
 enum error read_print_loop(struct cstream *cs, struct vec *line,
-                           struct parser *parser)
+        struct parser *parser, struct opts *opts)
 {
     enum error err;
     struct vec *final = NULL;
 
-    while (true)
+    if (!(opts->c))
     {
-        // Read the next character
-        int c;
-        if ((err = cstream_pop(cs, &c)))
-            return err;
-
-        // If the end of file was reached, stop right there
-        if (c == EOF)
-            break;
-
-        // If a newline was met, print the line
-        if (c == '\n')
+        while (true)
         {
-            final = vec_concat(final, line);
-            final->size--;
-            vec_push(final, '\n');
-            vec_push(final, '\0');
-            vec_reset(line);
-            continue;
+            // Read the next character
+            int c;
+            if ((err = cstream_pop(cs, &c)))
+                return err;
+
+            // If the end of file was reached, stop right there
+            if (c == EOF)
+                break;
+
+            // If a newline was met, print the line
+            if (c == '\n')
+            {
+                final = vec_concat(final, line);
+                final->size--;
+                vec_push(final, '\n');
+                vec_push(final, '\0');
+                vec_reset(line);
+                continue;
+            }
+
+            // Otherwise, add the character to the line
+            vec_push(line, c);
         }
-
-        // Otherwise, add the character to the line
-        vec_push(line, c);
+        vec_destroy(line);
     }
-    vec_destroy(line);
-
+    else
+    {
+        final = vec_init();
+        final->data = strdup(opts->input);
+        final->size = strlen(opts->input);
+        final->capacity = strlen(opts->input);
+    }
     parser->lexer = lexer_create(vec_cstring(final));
     enum parser_state state = parsing(parser);
     if (state != PARSER_OK)
@@ -82,7 +124,9 @@ enum error read_print_loop(struct cstream *cs, struct vec *line,
         free(final);
         return PARSER_ERROR;
     }
-    pretty_print(parser->ast);
+    if (opts->p)
+        pretty_print(parser->ast);
+    ast_eval(parser->ast);
     vec_destroy(final);
     free(final);
     return NO_ERROR;
@@ -93,24 +137,27 @@ int main(int argc, char *argv[])
     int rc = 0;
 
     // Parse command line arguments and get an input stream
-    struct cstream *cs;
-    if ((cs = parse_args(argc, argv)) == NULL)
-        return 1;
+    struct opts *opts = NULL;
+    struct cstream *cs = parse_args(argc, argv, &opts);
 
     // Create a vector to hold the current line
     struct vec *line = vec_init();
 
     struct parser *parser = create_parser();
     // Run the test loop
-    if (read_print_loop(cs, line, parser) != NO_ERROR)
+    if (read_print_loop(cs, line, parser, opts) != NO_ERROR)
     {
         rc = 1;
         vec_destroy(line);
     }
 
+    free(opts);
     free(line);
-    cstream_free(cs);
-    free(cs);
+    if (cs)
+    {
+        cstream_free(cs);
+        free(cs);
+    }
     parser_free(parser);
     return rc;
 }
--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -7,21 +7,11 @@
 #include <string.h>
 #include <utils/alloc.h>
 #include <utils/vec.h>
+#include <utils/utils.h>
 
 #define SIZE 8
 
 enum state state = DEFAULT; // Save the current state of the Lexer
-
-static int is_separator(char c)
-{
-    char separator[5] = ";|\0\n "; // Array of possible separator
-    for (size_t i = 0; i < 5; i++)
-    {
-        if (c == separator[i])
-            return 1;
-    }
-    return 0;
-}
 
 static int match_token(char *str, int quote)
 {
