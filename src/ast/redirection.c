ccb66fd8afd03ded8cb548cb06fcf53bc5399147
Nolab <thibault.boutet@epita.fr>
2021-12-01 09:56:14 +0100

    fix restoring fd

--- src/ast/redirection.c
+++ src/ast/redirection.c
@@ -8,12 +8,16 @@
 
 int redir_simple_left(char *left, int fd, char *right)
 {
+    int save_fd = dup(fd);
+
     int file_fd = open(right, O_CREAT | O_TRUNC | O_WRONLY, 0644);
     if (file_fd == -1)
         return -1;
     if (dup2(file_fd, fd) == -1)
         return -1;
     int r_code = cmd_exec(left);
+    fflush(stdout);
+    dup2(save_fd, fd); // Restore file descriptor
     close(file_fd);
     return r_code;
 }
